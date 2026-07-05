fd2be08c6fe40017bb790436eef0f1763fa8063f
md-abu-kayser <abu.kayser.official@gmail.com>
2026-07-06 00:21:51 +0600

    feat: add async error handler wrapper for Express routes

--- 15-code-snippets/async-handler.ts
+++ 15-code-snippets/async-handler.ts
@@ -1,2 +1,13 @@
-export const asyncHandler = (fn: any) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
+import { Request, Response, NextFunction } from "express";
+
+type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => Promise<any>;
+
+export const asyncHandler = (fn: AsyncRequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
